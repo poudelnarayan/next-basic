bbf5ceebd6e753f3dd17c853bed9d143133c132f
Narayan Poudel <narayanpoudel@Narayans-MacBook-Pro.local>
2025-07-19 20:46:10 +0545

    good way of adding link

--- app/page.js
+++ app/page.js
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main>
@@ -5,8 +7,7 @@ export default function Home() {
       <h1>Welcome to this NextJS Course!</h1>
       <p>🔥 Let&apos;s get started! 🔥</p>
       <p>
-        {" "}
-        <a href="/about">About Us </a>
+        <Link href="/about">About Us </Link>
       </p>
     </main>
   );
